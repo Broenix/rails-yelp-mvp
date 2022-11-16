d7ceedd4e55208462306e6c42bb9512e6bfbbc14
theo chevalier <chevalie.theo@gmail.com>
2022-11-16 16:44:08 +0100

    Ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning restaurants..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+sandwichman = {name: "Sandwichman", address: "Jouy-en-Josace", phone_number: "0554332556", category:"french"}
+burgerman = {name: "Burgerman", address: "Moncuq", phone_number: "0534532556", category:"belgian"}
+sushiman = {name: "Sushiman", address: "Japon-city", phone_number: "0654332556", category:"japanese"}
+spaghettiman = {name: "Spaghettiman", address: "Mamamiaville", phone_number: "0554382556", category:"italian"}
+chineseman = {name: "Chineseman", address: "Dunkerque", phone_number: "0554332506", category:"chinese"}
+
+[sandwichman, burgerman, sushiman, spaghettiman, chineseman].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
